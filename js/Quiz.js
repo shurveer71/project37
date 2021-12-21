7bf0752e94926f26695a6578ddfba9ff4ab45e03
shurveer71 <90134527+shurveer71@users.noreply.github.com>
2021-12-21 15:50:16 +0530

    Add files via upload

---
+++ js/Quiz.js
@@ -0,0 +1,10 @@
+class Quiz {
+  constructor(){}
+
+  start(){
+      //this.start= new this.start
+      question = new Question()
+      question.display();
+      
+  } 
+}
---
+++ sketch.js
@@ -0,0 +1,24 @@
+var canvas, backgroundImage;
+
+var questions;
+
+var question, contestant, quiz;
+
+
+function setup(){
+  canvas = createCanvas(850,400);
+  database = firebase.database();
+  quiz = new Quiz();
+  question= new Question();
+  //quiz.getState()
+//call the start() method inside the quiz class
+  quiz.start()
+
+}
+ 
+
+function draw(){
+  background("pink");
+  
+ 
+}
